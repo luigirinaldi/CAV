3a086931f3106bfde3cb281e1b31b4e728dc0632
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-04-22 11:44:25 +0100

    recreating tables

--- scripts/plots/utils.py
+++ scripts/plots/utils.py
@@ -68,11 +68,16 @@ def load_sledge_data(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
 
-    times = {"name":[], "time":[], "timeout":[]}
+    times = {"name":[], "time":[], "timeout":[], "proof": []}
     for theorem, entry in data.items():
         times["name"].append(theorem)
-        times["timeout"].append("proof" in entry[0])
-        times["time"].append(entry[0]["total_time"])
+        timeout = "proof" not in entry[0]
+        times["timeout"].append(timeout)
+        times["time"].append(entry[0]["total_time"] if timeout else entry[0]['atp_time'])
+        if not timeout:
+            times['proof'].append(entry[0]['proof'])
+        else:
+            times['proof'].append(None)
     return pd.DataFrame(times)
 
 # Load times from E-Graph file
@@ -92,3 +97,96 @@ def prepare_step_data(series):
         x.extend([t, t])
         y.extend([y[-1], i])
     return x, y
+
+def plot_cactus(ax, dataframe, timeout=None, pltopts=None):
+    num_problems = len(dataframe)
+
+    ax.axhline(
+        num_problems,
+        label=f"Number of problems: {num_problems}",
+        color="black",
+        linestyle="dotted",
+    )
+    if timeout:
+        ax.axvline(
+            timeout,
+            label=f"Timeout: {timeout / 1000:.3g} Seconds",
+            color="grey",
+            linestyle="--",
+        )
+
+    solved_counts = []
+    for name, values in dataframe.items():
+        solved = values.dropna()
+        x, y = prepare_step_data(solved.to_list())
+
+        if timeout and x[-1] < timeout:
+            y.append(y[-1])
+            x.append(timeout)
+
+        line_opts = pltopts.get(name, {}) if pltopts else {}
+
+        ax.plot(
+            x,
+            y,
+            drawstyle="steps-post",
+            label=f"{name}: {len(solved)} (${100 * len(solved) / num_problems:.0f}\\%$) ",
+            **line_opts,
+        )
+        solved_counts.append(len(solved))
+
+    ax.set_xlabel("Runtime (ms)")
+    ax.set_ylabel("Number of solved problems")
+    ax.set_xscale("log")
+
+    y_ticks = ax.get_yticks()
+    y_ticks = [*y_ticks, num_problems] if num_problems not in y_ticks else y_ticks
+    ax.set_yticks(sorted(y_ticks))
+
+    ax2 = ax.twinx()
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_yticks(sorted(set(solved_counts)))
+    ax2.set_ylabel("Solved instances")
+
+
+def plot_survival(ax, dataframe, timeout=None, pltopts=None):
+    num_problems = len(dataframe)
+
+    ax.axvline(num_problems, label=f"Number of problems: {num_problems}", color="blue")
+    if timeout:
+        ax.axhline(
+            timeout,
+            label=f"Timeout: {timeout / 1000:.3g} Seconds",
+            color="grey",
+            linestyle="--",
+        )
+
+    for name, values in dataframe.items():
+        solved = values.dropna()
+        y, x = prepare_step_data(solved.to_list())
+
+        if timeout and x[-1] < num_problems:
+            x.append(x[-1])
+            y.append(timeout)
+
+        line_opts = pltopts.get(name, {}) if pltopts else {}
+
+        ax.plot(
+            x,
+            y,
+            drawstyle="steps-post",
+            label=f"{name}: {len(solved)} (${100 * len(solved) / num_problems:.0f}\\%$) ",
+            **line_opts,
+        )
+
+    ax.set_ylabel("Runtime (ms)")
+    ax.set_xlabel("Number of solved problems")
+    ax.set_yscale("log")
+
+
+def combine_results(x):
+    vals_as_list = x.dropna().to_list()
+    if vals_as_list != []:
+        return min(x.dropna().to_list())
+    else:
+        return None
