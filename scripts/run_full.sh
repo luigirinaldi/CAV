3e33ac86badd5214506b05779f55939664a54c3d
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-04-23 09:50:19 +0100

    run verify on results

--- scripts/run_full.sh
+++ scripts/run_full.sh
@@ -12,7 +12,7 @@ uv run collect_parabit.py -j 2 -m 8 -t 60 --outdir ../results
 uv run collect_pbv.py -j 8 -t 60 --outdir ../results
 
 # Run ROVER with verify to check Isabelle
-uv run collect_parabit.py -j 2 -m 8 -t 600 --outdir ../results
+uv run collect_parabit.py -j 2 -m 8 -t 600 --outdir ../results --verify
 
 # Generate Tables and Plots
 uv run jupyter nbconvert --to notebook --execute --inplace ./plots/eval_graphs.ipynb
--- scripts/run_small.sh
+++ scripts/run_small.sh
@@ -12,7 +12,7 @@ uv run collect_parabit.py -j 8 -m 2 -t 5 --outdir ../results_small
 uv run collect_pbv.py -j 8 -t 5 --outdir ../results_small
 
 # Run ROVER with verify to check Isabelle
-uv run collect_parabit.py -j 8 -m 2 -t 10 --outdir ../results_small
+uv run collect_parabit.py -j 8 -m 2 -t 10 --outdir ../results_small --verify
 
 # Generate Tables and Plots
 RESULTS_DIR='../results_small' uv run jupyter nbconvert --to notebook --execute --inplace ./plots/eval_graphs.ipynb
