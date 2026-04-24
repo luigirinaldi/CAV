5e98875c8dfc3a1497f466ce8e6cad99214e7211
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-04-24 10:40:19 +0100

    get pbv during smoke test and table 3

--- scripts/run_smoke.sh
+++ scripts/run_smoke.sh
@@ -12,8 +12,9 @@ uv run parabit_runner.py ../benchmarks/Hydra/bwlang ../smoke_test/parabit/Hydra 
 uv run pbv_runner.py ../benchmarks/Alive/smt2 ../smoke_test/pbv/Alive --max-workers 8 --timeout 1
 uv run pbv_runner.py ../benchmarks/Hydra/smt2 ../smoke_test/pbv/Hydra --max-workers 8 --timeout 1
 
-# Run ROVER with verify to check Isabelle
+# Run Alive and Hydra with verify to check Isabelle
 uv run parabit_runner.py ../benchmarks/Alive/bwlang ../smoke_test/parabit_verif/Alive -j 8 -m 1 -t 1 --check-isabelle
+uv run parabit_runner.py ../benchmarks/Hydra/bwlang ../smoke_test/parabit_verif/Hydra -j 8 -m 1 -t 1 --check-isabelle
 
 # Generate Tables and Plots
 TIMEOUT=1000 SMOKE_TEST=1 uv run jupyter nbconvert --to notebook --execute --inplace ./plots/eval_graphs.ipynb
