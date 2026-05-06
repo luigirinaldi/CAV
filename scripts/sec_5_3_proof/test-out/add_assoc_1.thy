a21c47bd79d8678e7aa0eb4723310bd9f4bcbd3f
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-05-06 17:50:44 +0100

    rewrite it a readable way

--- scripts/sec_5_3_proof/test-out/add_assoc_1.thy
+++ scripts/sec_5_3_proof/test-out/add_assoc_1.thy
@@ -1,15 +1,26 @@
 theory add_assoc_1
     imports  arith_lemmas
 begin
-theorem add_assoc_1_th:
-"(bw r ((bw q ((bw p a) + (bw p b))) + (bw p c)))=(bw r ((bw p a) + (bw q ((bw p b) + (bw p c)))))" (is "?lhs = ?rhs")
-if "(q >= r)" and "(r > 0)" and "(p > 0)" and "(q > 0)"
-for r p q :: nat and b c a :: int
+
+theorem motivational_example:
+  fixes p q r :: nat 
+  fixes a b c :: int
+  assumes "p > 0" and "r > 0" and "q > 0"
+  assumes "q >= r"
+  shows "(bw r ((bw q ((bw p a) + (bw p b))) + (bw p c)))
+        = (bw r ((bw p a) + (bw q ((bw p b) + (bw p c)))))" 
+        (is "?lhs = ?rhs")
 proof -
-    have "?lhs = (bw r (((bw p a) + (bw p b)) + (bw p c)))" using add_remove_prec_left that by (simp ; fail | simp only: add_remove_prec_left; fail | simp add: add_remove_prec_left; fail | blast; fail | metis)
-    moreover have "... = (bw r ((bw p a) + ((bw p b) + (bw p c))))" using add.assoc that by (simp ; fail | simp only: add.assoc; fail | simp add: add.assoc; fail | blast; fail | metis)
-    moreover have "... = (bw r ((bw p a) + (bw q ((bw p b) + (bw p c)))))" using add_remove_prec_right that by (simp ; fail | simp only: add_remove_prec_right; fail | simp add: add_remove_prec_right; fail | blast; fail | metis)
-ultimately show ?thesis by argo
+  have "?lhs = (bw r (((bw p a) + (bw p b)) + (bw p c)))" 
+    using add_remove_prec_left assms 
+    by (simp ; fail | simp only: add_remove_prec_left; fail | simp add: add_remove_prec_left; fail | blast; fail | metis)
+  moreover have "... = (bw r ((bw p a) + ((bw p b) + (bw p c))))" 
+    using add.assoc assms 
+    by (simp ; fail | simp only: add.assoc; fail | simp add: add.assoc; fail | blast; fail | metis)
+  moreover have "... = (bw r ((bw p a) + (bw q ((bw p b) + (bw p c)))))" 
+    using add_remove_prec_right assms 
+    by (simp ; fail | simp only: add_remove_prec_right; fail | simp add: add_remove_prec_right; fail | blast; fail | metis)
+  ultimately show ?thesis by argo
 qed
 
 end
